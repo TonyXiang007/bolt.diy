950768d0dea00813a036e3331fd15d6d37f70700
TonyXiang007 <22788489@qq.com>
2025-02-17 23:39:51 +0800

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -506,7 +506,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="How can AI Agent help you today?"
+                      placeholder="How can AI agent help you today?"
                       translate="no"
                     />
                     <ClientOnly>
